0fc853d7cb783181bc51a90031dcf4f9355fec6a
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2025-05-10 22:46:43 +0700

    Clean up files of external folders

--- local_api/TTSAPI.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-
-
-class TTS_API:
-    def GenerateVoice(self, text: str, path: str, lang: str = "en", slowed: bool = False) -> None:
-        sound_object = gTTS(text, lang, slowed)
-        sound_object.save(path)
